4e4736321a2de0adbb2376531e52fe1412636244
Anandghan W <awaghmare3@gatech.edu>
2017-03-18 02:27:06 -0400

    [Mobile] Added UI for login and registration

--- mobile/PartyPlaylist/app/src/main/java/edu/gatech/seclass/partyplaylist/Login.java
+++ mobile/PartyPlaylist/app/src/main/java/edu/gatech/seclass/partyplaylist/Login.java
@@ -2,12 +2,18 @@ package edu.gatech.seclass.partyplaylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+        setContentView(R.layout.login);
+        setTitle("Login");
+    }
+
+    public void login(View view){
+
     }
 }
---
+++ mobile/PartyPlaylist/app/src/main/java/edu/gatech/seclass/partyplaylist/register.java
@@ -0,0 +1,23 @@
+package edu.gatech.seclass.partyplaylist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by andy on 3/18/17.
+ */
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        setTitle("Register");
+    }
+
+    public void register(View view) {
+
+    }
+}
