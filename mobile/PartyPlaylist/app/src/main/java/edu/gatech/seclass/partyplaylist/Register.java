2e81cb28018fa1f6bed0692bce4f331a722b99f0
richard <lichard49@gmail.com>
2017-03-19 10:58:38 -0400

    [Mobile] Refactored UI changes to make the app compile-able...

--- mobile/PartyPlaylist/app/src/main/java/edu/gatech/seclass/partyplaylist/Register.java
+++ mobile/PartyPlaylist/app/src/main/java/edu/gatech/seclass/partyplaylist/Register.java
@@ -13,7 +13,7 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.register_layout);
         setTitle("Register");
     }
 
