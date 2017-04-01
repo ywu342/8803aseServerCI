9eeefcae10d9aaa532192dc5698e74982f3ee636
Cao, Hong <hcao40@gatech.edu>
2017-04-01 13:58:23 -0400

    Add files via upload

--- backend/UserRegistration/src/com/gatech/userreg/UserRegistrationServlet.java
+++ backend/UserRegistration/src/com/gatech/userreg/UserRegistrationServlet.java
@@ -12,25 +12,29 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class UserRegistrationServlet extends HttpServlet {
 	public static final Logger _logger = Logger.getLogger(UserRegistrationServlet.class.getName());
-
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String name = "";
-		String strResponse = "";
 		String email = "";
 		String password = "";
+		UserError response = new UserError();
 		try {
 			email = req.getParameter("email");
 			password = req.getParameter("password");
 			name = req.getParameter("name");
 
 			User usr = DBUtilsUser.getUser(name, email, password);
-			strResponse = "User Found: " + usr.getEmail() + " " + usr.getName() + " " + usr.getPassword();
+			response.setCode(200);
+			response.setData(usr);
+			response.setDescrip("User Found");
+			
 		} catch (Exception ex) {
-
-			strResponse = "Error in retrieving user. " + ex.getMessage();
+			response.setCode(400);
+			response.setDescrip("Error in retrieving user. " + ex.getMessage());
+			
 		}
-		resp.getWriter().println(strResponse);
+		resp.getWriter().println(response);
 	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -42,18 +46,20 @@ public class UserRegistrationServlet extends HttpServlet {
 
 		reader.close();
 
-		String strResponse = "";
-
+		UserError response = new UserError();
+		response.setData(usr);
+		
 		try {
-
 			DBUtilsUser.saveUser(usr);
-			strResponse = "Your account has been created.";
+			response.setCode(100);
+			response.setDescrip("Your account has been created");
 		} catch (Exception ex) {
 			_logger.severe("Error in creating account : " + usr.getName() + "," + usr.getEmail() + " : "
 					+ usr.getPassword() + ex.getMessage());
-			strResponse = "Error in saving User via web. Reason : " + ex.getMessage();
+			response.setCode(200);
+			response.setDescrip("Error in saving User via web. Reason :" + ex.getMessage());
 		}
-		resp.getWriter().println(strResponse);
+		resp.getWriter().println(response);
 
 	}
 }
